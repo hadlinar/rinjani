bf38fb23db183b61119cfd806ef5e5ed0164f76e
hadlinar <hadlinar@gmail.com>
2022-05-30 16:40:02 +0700

    stuck pdf

---
+++ rinjani/lib/utils/mobile.dart
@@ -0,0 +1,12 @@
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'dart:io';
+
+Future<void> saveAndLaunchFile(List<int> bytes, String fileName) async {
+  final path = (await getExternalStorageDirectory())!.path;
+  final file = File('$path/$fileName');
+  await file.writeAsBytes(bytes, flush: true);
+  print("test");
+  OpenFile.open('$path/$fileName');
+}
